ce7644cfaff5064b296fdf6ae2ab7bb7545a56a7
Maarten Claes <maartencls@gmail.com>
2014-03-01 18:51:32 +0100

    Update builds

--- dist/angular-active_href.js
+++ dist/angular-active_href.js
@@ -2,12 +2,26 @@
 angular.module('activeHref', []).directive('activeHref', [
   '$location',
   function ($location) {
-    var link = function (scope, element, attrs) {
-      if (!attrs.href) {
-        return;
+    var matchesCustom = function (pattern) {
+      if (!pattern) {
+        return false;
       }
-      var href = attrs.href.replace(/^\/#/, '');
-      if ($location.path() === href) {
+      var regex = new RegExp(pattern);
+      return regex.test($location.path());
+      return false;
+    };
+    var matchesHref = function (href) {
+      if (!href) {
+        return false;
+      }
+      var path = href.replace(/^\/#/, '');
+      return $location.path() === path;
+    };
+    var matchesPath = function (attrs) {
+      return matchesHref(attrs.href) || matchesCustom(attrs.routeMatcher);
+    };
+    var link = function (scope, element, attrs) {
+      if (matchesPath(attrs)) {
         element.addClass('active');
       }
     };
